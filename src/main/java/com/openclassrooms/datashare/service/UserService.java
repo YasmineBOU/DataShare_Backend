e1f6c201d59cba6ace37ac99ed3e8cc4a4bbdc91
YasmineBOU <yasmineboudjemai10@gmail.com>
2026-06-09 20:17:18 +0200

    --amend

--- src/main/java/com/openclassrooms/datashare/service/UserService.java
+++ src/main/java/com/openclassrooms/datashare/service/UserService.java
@@ -38,7 +38,6 @@ import org.springframework.util.Assert;
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
-    private static final Logger log = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
